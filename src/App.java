4b4f3bda09bc47686b2ddafde078cba289d9b2e0
raquel <raquelrestrepo.2003@gmail.com>
2025-11-09 18:07:12 -0500

    modificacion segunda clase

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola soy yo");
+        System.out.println("Hola soy raquel y esta es la segunda clase");
     }
 }
